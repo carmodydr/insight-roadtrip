b35de75c09f6821f959e9dec0e85e79bbd63374d
Dan Carmody <carmody.d.r@gmail.com>
2016-06-28 13:17:47 -0700

    secrets

--- web/flask_roadtrip/views.py
+++ web/flask_roadtrip/views.py
@@ -9,65 +9,96 @@ from events import returnTopEvents
 from geocode import latlon
 from rankingSim import artistPath
 import networkx as nx
-
-username = 'dan'
-host = 'localhost'
-dbname = 'music'
-pswd = 'insight'
+import unicodedata
+import HTMLParser
+from secrets import username, host, dbname, pswd
 
 engine = create_engine('mysql://%s:%s@localhost/%s'%(username,pswd,dbname))
 con = None
 con = mdb.connect('localhost', username, pswd, dbname)
-simArtists = pd.read_sql_table('relArtist',con='mysql://%s:%s@localhost/%s'%(username,pswd,dbname))
+simArtists = pd.read_sql_table('relArtistFull',con='mysql://%s:%s@localhost/%s'%(username,pswd,dbname))
 g = nx.from_pandas_dataframe(simArtists,'Artist','RelArtist')
 
 
 
-@app.route('/')
-@app.route('/index/')
-def index():
-	user = { 'nickname': 'Dan' } # fake user
-	return render_template("index.html",
-		title = 'Home',
-		user = user)
+#@app.route('/')
+#@app.route('/index/')
+#def index():
+#	user = { 'nickname': 'Dan' } # fake user
+#	return render_template("index.html",
+#		title = 'Home',
+#		user = user)
 
-@app.route('/input')
+@app.route('/')
+@app.route('/index')
 def roadtrip_input():
 	return render_template("input.html")
+
+@app.route('/about')
+def roadtrip_about():
+	return render_template("about.html")
 
 @app.route('/map')
 def map():
 	start = request.args.get('start_location')
-	startlat, startlon = latlon(start)
-
+	startdate = request.args.get('start_date')
 	end = request.args.get('end_location')
+	enddate = request.args.get('end_date')
+	seedArtist = request.args.get('seed_artist')
+	relWeight = request.args.get('points')
+	
+	startlat, startlon = latlon(start)
+	
 	endlat, endlon = latlon(end)
 
-	seedArtist = request.args.get('seed_artist')
+#	try:
+#		temp = pd.to_datetime(startDate)
+#		temp = pd.to_datetime(endDate)
+#	except:
+#		return render_template("date_error.html")
+
 	events = []
 	topEvents = []
 
-	topEvents = returnTopEvents( startlat, startlon, endlat, endlon, '100', '2016-07-06', '2016-07-14', seedArtist, g) 
+	radius = '100'
+
+	topEvents = returnTopEvents( startlat, startlon, endlat, endlon, radius, startdate, enddate, seedArtist, g, float(relWeight)) 
 
 	placesList = []
+	bandList = []
+	venueList = []
+	eventList = []
+	parser = HTMLParser.HTMLParser()
 	for i in topEvents:
 		placesList.append( [i['venLat'], i['venLon']] )
 
 #	placesList = [[37,-70], [38.913184,-77.031952], [37.775408,-122.413682] , [42, -80]]
-	return render_template("map.html", placesList = placesList)
+	print bandList
+	return render_template("map.html", placesList = placesList, eventList = topEvents, startName = start, endName = end)
 
 @app.route("/output")
 def roadtrip_output():
-	start = request.args.get('start_location')
-	startlat, startlon = latlon(start)
+	try:
+		start = request.args.get('start_location')
+		startlat, startlon = latlon(start)
+		startdate = request.args.get('start_date')
+		
+		end = request.args.get('end_location')
+		endlat, endlon = latlon(end)
+		enddate = request.args.get('end_date')
 
-	end = request.args.get('end_location')
-	endlat, endlon = latlon(end)
-
-	seedArtist = request.args.get('seed_artist')
+		seedArtist = request.args.get('seed_artist')
+	except:
+		return render_template("error.html")
 	events = []
 	topEvents = []
 
-	topEvents = returnTopEvents( startlat, startlon, endlat, endlon, '100', '2016-07-06', '2016-07-14', seedArtist, g) 
+	radius = '150'
+
+	topEvents = returnTopEvents( startlat, startlon, endlat, endlon, radius, startdate, enddate, seedArtist, g) 
 
 	return render_template("output.html", events = topEvents)
+
+@app.route("/test")
+def test():
+	return render_template("test.html")
